ac939436b917481cef153130b0d25e5a6f16fde3
PreSuffix <vicentefranzini@gmail.com>
2022-05-18 20:01:39 -0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta property="og:image" content="http://web.archive.org/web/20220418171333im_/https://cdn.discordapp.com/attachments/950562809421385802/964708342754848849/gifgit.gif">
-<meta property="og:type" content="video.other">
-<meta property="og:video:url" content="http://web.archive.org/web/20220418171333/https://www.youtube.com/embed/83gGxgchfig">
-<meta property="og:video:width" content="1280">
-<meta property="og:video:height" content="720">
-</head>
-</html>
